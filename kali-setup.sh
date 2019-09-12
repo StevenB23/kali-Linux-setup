cf24c0f07ce63e4334a4ed3685f32ae94ad85bdb
Steven <darwinsgame@protonmail.com>
2019-09-11 23:39:14 -0700

    Update kali-setup.sh

--- kali-setup.sh
+++ kali-setup.sh
@@ -1,5 +1,13 @@
+echo "running apt-get update and installing kali packages"
 apt-get -q update
 cat kali-packages.txt | xargs apt-get install
+
+echo "creating ~/extra-tools directory"
 mkdir ~/extra-tools
+
+echo "installing Anaconda3-2019"
 wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh -P ~/extra-tools/
 bash ~/extra-tools/Anaconda3-2019.07-Linux-x86_64.sh -b
+
+#Append the export command to make conda and other commands available in the PATH var
+echo 'export PATH="/root/anaconda3/bin:$PATH"' >> ~/.bashrc 
