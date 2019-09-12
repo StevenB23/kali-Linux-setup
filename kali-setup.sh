169111c95456e7b5c9211a139fb3bffc486c597f
Steven <darwinsgame@protonmail.com>
2019-09-12 00:06:20 -0700

    Update kali-setup.sh

--- kali-setup.sh
+++ kali-setup.sh
@@ -1,18 +1,23 @@
-echo "running apt-get update and installing kali packages"
+# using tput commands to colorize the output.
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+echo "$(green)running apt-get update and installing kali packages ${reset}"
 apt-get -q update
 cat kali-packages.txt | xargs apt-get install
 
-echo "creating ~/extra-tools directory"
+echo "$(green)creating ~/extra-tools directory(reset)"
 mkdir ~/extra-tools
 
-echo "installing Anaconda3-2019"
+echo "$(green)installing Anaconda3-2019 $(reset)"
 wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh -P ~/extra-tools/
 bash ~/extra-tools/Anaconda3-2019.07-Linux-x86_64.sh -b
 
 #Append the export command to make conda and other commands available in my PATH var
 echo 'export PATH="/root/anaconda3/bin:$PATH"' >> ~/.bashrc 
-echo "restarting the shell with . ~/.bashrc"
+echo "$(green)restarting the shell with $(tput setaf 4). ~/.bashrc $(reset)"
 . ~/.bashrc
-echo "running conda init command"
+echo "$(green)running conda init command $(reset)"
 conda init 
-echo Jupyter and package installation completed
+echo "$(green)Jupyter and package installation completed $(reset)"
