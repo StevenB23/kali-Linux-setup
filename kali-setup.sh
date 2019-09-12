5786c69c3acea5172d381492352df5da8079ceb4
Steven <darwinsgame@protonmail.com>
2019-09-12 00:18:41 -0700

    Update kali-setup.sh

--- kali-setup.sh
+++ kali-setup.sh
@@ -3,22 +3,22 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-echo "$(green)running apt-get update and installing kali packages ${reset}"
+echo "${green}running apt-get update and installing kali packages ${reset}"
 apt-get -q update
 cat kali-packages.txt | xargs apt-get install
 
-echo "$(green)creating ~/extra-tools directory(reset)"
+echo "${green}creating ~/extra-tools directory{reset}"
 mkdir ~/extra-tools
 
-echo "$(green)installing Anaconda3-2019 $(reset)"
+echo "${green}installing Anaconda3-2019 ${reset}"
 wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh -P ~/extra-tools/
 bash ~/extra-tools/Anaconda3-2019.07-Linux-x86_64.sh -b
 
 #Append the export command to make conda and other commands available in my PATH var
 echo 'export PATH="/root/anaconda3/bin:$PATH"' >> ~/.bashrc 
-echo "$(green)restarting the shell with $(tput setaf 4). ~/.bashrc $(reset)"
+echo "${green}restarting the shell with $(tput setaf 4). ~/.bashrc ${reset}"
 . ~/.bashrc
-echo "$(green)running conda init command $(reset)"
+echo "${green}running conda init command ${reset}"
 conda init 
-echo "$(green)package installation completed $(reset)"
-echo "To Complete jupyter installation run $(red). ~/.bashrc; conda init; . ~/.bashrc$(reset)"
+echo "${green}package installation completed ${reset}"
+echo "To Complete jupyter installation run $(red). ~/.bashrc; conda init; . ~/.bashrc${reset}"
